9bfbb74e6d7da926d7dd56978e3744814fb7882d
Navenprasanth <87560415+Navenprasanth@users.noreply.github.com>
2021-07-28 00:08:20 -0700

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,20 @@
+from torbot.sample_config import config
+
+#read readme too before filling these stuffs
+
+class config(config):
+    TG_BOT_TOKEN= "1938067226:AAEHnFkt2qBCpqbvuPnnqVNY_RZvbvRh02Y" #imp
+    APP_ID = 6581231 #imp
+    API_HASH = "a0e709016292e83076deb8288fffd6f0" #imp
+    AUTH_CHANNEL =[1001523490381, 942182489] #imp replace your_id with your id from telegrm or delete
+    INDEX_LINK =""
+    GLEECH_COMMAND = "gleech@downloader91bot"
+    YTDL_COMMAND = 'ytdl@downloader91bot'
+    TELEGRAM_LEECH_COMMAND_G = "tleech@downloader91bot"
+    CLONE_COMMAND_G = "gclone"
+    PYTDL_COMMAND_G = "pytdl"
+    DESTINATION_FOLDER = "gdrive bot"
+    LEECH_COMMAND = "leech@torhunter2_bot"
+    CANCEL_COMMAND_G ="cancel@downloader91bot"
+    RCCLONE_CONFIG = """"
+    #input your config(replacing new line with \n) in the triple quote like above: """<your one liner config>"""
