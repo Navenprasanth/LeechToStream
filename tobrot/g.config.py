e7d276e0c420bffe0c08893bb4289036cc3e5288
Navenprasanth <87560415+Navenprasanth@users.noreply.github.com>
2021-07-28 01:18:08 -0700

    Create g.config.py

---
+++ tobrot/g.config.py
@@ -0,0 +1,25 @@
+from torbot.sample_config import config
+
+#fill yoyour all data,  read readme for reference
+
+#FOR CUSTOM COMMANDS READ README AND FILL THEM...
+
+class Config(config):
+    TG_BOT_TOKEN= "1268fill--- our data"
+    APP_ID = 137398
+    API_HASH =  "a0e709016292e83076deb8288fffd6f0" #imp
+    AUTH_CHANNEL = [ -1001523490381] #imp replace your_id with your id from telegram or delete
+    OWNER_ID =  942182489
+    DESTINATION_FOLDER ="torrentleech-Gdrive #Name of your folder read readme(not id of the folder)
+    #fill your rclone config like this(your config may have some extra value or less. son don't worry)
+    #Do not delete [DRIVE] #do not delete [DRIVE] but replace remaining part
+    RCLONE_CONFIG = """
+[DRIVE]
+type = drive
+client_id =28dslkgjsdl-fill-your-datails-apps.googleusercontent.com
+clinet_secret = 6Tib48-fill-your-details-KuXXDX-eWgnRBYc
+scope = drive
+root_folder_id =
+token -{"access_token":"ya29.a-fill-your-details-af4ychuHswBt8X0nf2oWmczHg6MYPSE6hXo-pJ-fill-your-details-s0KAecfw_H-tYTh8tbs:20:25.4309203152"}
+team_drive = 0AB0q-fill-your-details-sdrgsgsduk9PVA
+"""
